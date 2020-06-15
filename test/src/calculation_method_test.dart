c3dfa383572867efb91d10fe867cbd96ee0fb8cd
iamriajul <kmriajulislami@gmail.com>
2020-06-15 21:31:20 +0600

    test: Test CalculationMethod.getParameters

--- test/src/calculation_method_test.dart
+++ test/src/calculation_method_test.dart
@@ -1,4 +1,5 @@
 import 'package:adhan/adhan.dart';
+import 'package:dart_numerics/dart_numerics.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -9,5 +10,30 @@ void main() {
 
     expect(karachiMethod1 == karachiMethod2, isTrue);
     expect(karachiMethod1 == egyptianMethod1, isFalse);
+  });
+
+  test('Test CalculationMethod.getParameters', () {
+    CalculationParameters params;
+
+    params = CalculationMethod.muslim_world_league.getParameters();
+    expect(almostEqualD(params.fajrAngle, 18, 0.000001), isTrue);
+    expect(almostEqualD(params.ishaAngle, 17, 0.000001), isTrue);
+    expect(params.ishaInterval, 0);
+    expect(params.method, CalculationMethod.muslim_world_league);
+
+    params = CalculationMethod.egyptian.getParameters();
+    expect(almostEqualD(params.fajrAngle, 20, 0.000001), isTrue);
+    expect(almostEqualD(params.ishaAngle, 18, 0.000001), isTrue);
+    expect(params.ishaInterval, 0);
+    expect(params.method, CalculationMethod.egyptian);
+
+    params = CalculationMethod.other.getParameters();
+    expect(almostEqualD(params.fajrAngle, 0, 0.000001), isTrue);
+    expect(almostEqualD(params.ishaAngle, 0, 0.000001), isTrue);
+    expect(params.ishaInterval, 0);
+    expect(params.method, CalculationMethod.other);
+
+    CalculationMethod calculationMethod;
+    expect(() => calculationMethod.getParameters(), throwsFormatException);
   });
 }
