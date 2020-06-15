0e45cc75c994ec73a11d3ecaca1592f31425b2c1
iamriajul <kmriajulislami@gmail.com>
2020-06-15 13:50:16 +0600

    test: added calendar_util_test

--- lib/src/data/calendar_util.dart
+++ lib/src/data/calendar_util.dart
@@ -4,35 +4,38 @@ import 'date_components.dart';
 
 class CalendarUtil {
   /// Whether or not a year is a leap year (has 366 days)
-  /// @param year the year
-  /// @return whether or not its a leap year
+  ///
+  /// [year] the year
+  /// return [bool] whether or not its a leap year
   static bool isLeapYear(int year) {
     return year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0);
   }
 
   /// Date and time with a rounded minute
+  ///
   /// This returns a date with the seconds rounded and added to the minute
-  /// @param when the date and time
-  /// @return the date and time with 0 seconds and minutes including rounded seconds
-  static DateTime roundedMinute(DateTime _when) {
-    final when = _when;
+  /// [when] the date and time
+  /// return [DateTime] and time with 0 seconds and minutes including rounded seconds
+  static DateTime roundedMinute(DateTime when) {
     final minute = when.minute;
     final second = when.second;
     return when.copyWith(minute: (minute + (second / 60).round()), second: 0);
   }
 
-  /// Gets a date for the particular date
-  /// @param components the date components
-  /// @return the date with a time set to 00:00:00 at utc
+  /// Gets a UTC DateTime for the particular date
+  ///
+  /// [components] the date components
+  /// return [DateTime] with a time set to 00:00:00 at utc
   static DateTime resolveTimeByDateComponents(DateComponents components) {
     return resolveTime(components.year, components.month, components.day);
   }
 
-  /// Gets a date for the particular date
-  /// @param year the year
-  /// @param month the month
-  /// @param day the day
-  /// @return the date with a time set to 00:00:00 at utc
+  /// Gets UTC DateTime for the particular date
+  ///
+  /// [year] the year
+  /// [month] the month
+  /// [day] the day
+  /// return [DateTime] with a time set to 00:00:00 at utc
   static DateTime resolveTime(int year, int month, int day) {
     return DateTime.utc(year, month, day, 0, 0, 0);
   }
---
+++ test/src/data/calendar_util_test.dart
@@ -0,0 +1,26 @@
+import 'package:adhan/adhan.dart';
+import 'package:adhan/src/data/calendar_util.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Test CalenderUtil.isLeapYear', () {
+    expect(CalendarUtil.isLeapYear(2000), isTrue);
+    expect(CalendarUtil.isLeapYear(2001), isFalse);
+  });
+
+  test('Test CalenderUtil.roundedMinute', () {
+    expect(CalendarUtil.roundedMinute(DateTime(2020, 1, 1, 1, 1, 50)),
+        DateTime(2020, 1, 1, 1, 2, 0));
+    expect(CalendarUtil.roundedMinute(DateTime(2020, 1, 1, 1, 1, 29)),
+        DateTime(2020, 1, 1, 1, 1, 0));
+  });
+
+  test('Test CalenderUtil.resolveTimeByDateComponents', () {
+    expect(CalendarUtil.resolveTimeByDateComponents(DateComponents(2020, 1, 1)),
+        DateTime.utc(2020, 1, 1));
+  });
+
+  test('Test CalenderUtil.resolveTime', () {
+    expect(CalendarUtil.resolveTime(2020, 1, 1), DateTime.utc(2020, 1, 1));
+  });
+}
