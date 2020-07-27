5e40e8e6e2a73766e83d006e8767c8623425db9b
iamriajul <kmriajulislami@gmail.com>
2020-07-27 12:00:38 +0600

    feat: Coordinates validation support, via a optional validate boolean param.

--- lib/src/coordinates.dart
+++ lib/src/coordinates.dart
@@ -2,5 +2,17 @@ class Coordinates {
   final double latitude;
   final double longitude;
 
-  Coordinates(this.latitude, this.longitude);
+  Coordinates(this.latitude, this.longitude, {bool validate = false}) {
+    if (validate) {
+      if (!(latitude.isFinite && latitude.abs() <= 90)) {
+        throw ArgumentError(
+            'Invalid Latitude supplied, Latitude must be a number between -90 and 90');
+      }
+
+      if (!(longitude.isFinite && longitude.abs() <= 180)) {
+        throw ArgumentError(
+            'Invalid longitude supplied, Longitude must a number between -180 and 180');
+      }
+    }
+  }
 }
