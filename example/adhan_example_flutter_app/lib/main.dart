3b74bab3921d173551bdf3c9fa246e0eaf5bb7d2
iamriajul <kmriajulislami@gmail.com>
2021-01-11 09:15:45 +0600

    fix: Unhandled Exception: setState() called after dispose():

--- example/adhan_example_flutter_app/lib/main.dart
+++ example/adhan_example_flutter_app/lib/main.dart
@@ -73,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     getLocationData().then((locationData) {
-      if (mounted) {
+      if (!mounted) {
         return;
       }
       if (locationData != null) {
