313ed8eb2e7423c9dbd65ef9665bcf4481b1b5f4
iamriajul <kmriajulislami@gmail.com>
2021-04-03 15:21:07 +0600

    fix: exclude MadhabExtensions from exporting.

--- test/src/madhab_test.dart
+++ test/src/madhab_test.dart
@@ -10,7 +10,5 @@ void main() {
     expect(hanafiMadhab1 == hanafiMadhab2, isTrue);
     expect(hanafiMadhab1 == shafiMadhab1, isFalse);
 
-    Madhab madhab;
-    expect(() => madhab.getShadowLength(), throwsFormatException);
   });
 }
