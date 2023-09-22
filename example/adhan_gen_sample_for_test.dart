a8fe10df04d76cfd08c36067fa3d0dade077538c
Riajul Islam <kmriajulislami@gmail.com>
2023-09-22 18:33:03 +0600

    feat: script for generating sample data for test. (testing v1 & v2 result)

--- example/adhan_gen_sample_for_test.dart
+++ example/adhan_gen_sample_for_test.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:adhan/adhan.dart';
 import 'package:intl/intl.dart';
@@ -15,8 +16,29 @@ void main() async {
     timezone: 'Asia/Dhaka',
     parameters: CalculationMethod.karachi.getParameters()
       ..madhab = Madhab.hanafi,
-    from: DateTime(2021, 1, 1),
-    to: DateTime(2022, 1, 1),
+    dates: generateDates(
+      from: DateTime(2021, 1, 1),
+      to: DateTime(2022, 1, 1),
+    ),
+  );
+
+  /// Generate 1000 random dates within last 30 years
+  final randomDates = <DateTime>[];
+  final random = Random();
+  for (var i = 0; i < 1000; i++) {
+    final year = random.nextInt(30) + 1990;
+    final month = random.nextInt(12) + 1;
+    final day = random.nextInt(28) + 1;
+    randomDates.add(DateTime(year, month, day));
+  }
+
+  generate(
+    name: 'Kushtia-Karachi-Hanafi-Random-Dates-Generated-Using-V1',
+    coordinates: Coordinates(23.9088, 89.1220),
+    timezone: 'Asia/Dhaka',
+    parameters: CalculationMethod.karachi.getParameters()
+      ..madhab = Madhab.hanafi,
+    dates: randomDates,
   );
 
   generate(
@@ -25,9 +47,31 @@ void main() async {
     timezone: 'America/New_York',
     parameters: CalculationMethod.north_america.getParameters()
       ..madhab = Madhab.hanafi,
-    from: DateTime(2021, 1, 1),
-    to: DateTime(2022, 1, 1),
+    dates: generateDates(
+      from: DateTime(2021, 1, 1),
+      to: DateTime(2022, 1, 1),
+    ),
   );
+}
+
+List<DateTime> generateDates({
+  DateTime from,
+  DateTime to
+}) {
+  from = DateTime(from.year, from.month, from.day);
+  to = DateTime(to.year, to.month, to.day);
+
+  assert(from.isBefore(to));
+
+  final dates = <DateTime>[];
+
+  var date = from;
+  while (date.isBefore(to)) {
+    dates.add(date);
+    date = date.add(Duration(days: 1));
+  }
+
+  return dates;
 }
 
 void generate({
@@ -35,23 +79,12 @@ void generate({
   Coordinates coordinates,
   String timezone,
   CalculationParameters parameters,
-  DateTime from, // inclusive
-  DateTime to, // exclusive
+  List<DateTime> dates,
 }) {
-  from = DateTime(from.year, from.month, from.day);
-  to = DateTime(to.year, to.month, to.day);
-
-  assert(from.isBefore(to));
-
   // Get the timezone object using the timezone name
   final location = getLocation(timezone);
 
-  final times = <Map<String, String>>[];
-
-  var date = from;
-  while (date.isBefore(to)) {
-    date = date.add(const Duration(days: 1));
-
+  final times = dates.map((date) {
     final prayerTimes = PrayerTimes(
       coordinates,
       DateComponents.from(date),
@@ -65,7 +98,7 @@ void generate({
     final maghrib = TZDateTime.from(prayerTimes.maghrib, location);
     final isha = TZDateTime.from(prayerTimes.isha, location);
 
-    times.add({
+    return {
       'date': DateFormat('yyyy-MM-dd').format(date),
       'fajr': DateFormat.jm().format(fajr),
       'sunrise': DateFormat.jm().format(sunrise),
@@ -73,8 +106,8 @@ void generate({
       'asr': DateFormat.jm().format(asr),
       'maghrib': DateFormat.jm().format(maghrib),
       'isha': DateFormat.jm().format(isha),
-    });
-  }
+    };
+  }).toList();
 
   // write to file
   final file = File('test/data/prayer-times/$name.json');
